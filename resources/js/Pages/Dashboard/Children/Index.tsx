219bfa518778c26441ac22a1375d01954cc0751e
dimpram101 <dimas2004211@gmail.com>
2023-07-11 00:21:27 +0800

    style: restyled background shadow

--- resources/js/Pages/Dashboard/Children/Index.tsx
+++ resources/js/Pages/Dashboard/Children/Index.tsx
@@ -18,7 +18,7 @@ const Index = ({ children }: Props) => {
         <h1 className='font-sofia font-bold text-3xl'>Data Anak</h1>
         <LinkButton href={route('data-anak.create')} className='font-roboto'>Tambah Data Anak</LinkButton>
       </div>
-      <div className="mt-4 w-full h-fit shadow-xl border rounded-lg p-4">
+      <div className="mt-4 w-full h-fit p-4">
         <div className="flex flex-wrap justify-around h-fit w-full gap-4 gap mx-auto">
           {children.length > 0 && children.map((child) => <ChildDataCard key={child.id} child={child}/>)}
         </div>
