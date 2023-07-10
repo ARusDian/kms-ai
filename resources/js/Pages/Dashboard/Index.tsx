e8572fdc9b20c3c1ca2316fb1535b1286d71d937
dimpram101 <dimas2004211@gmail.com>
2023-07-11 01:40:27 +0800

    fix: fix naming web title

--- resources/js/Pages/Dashboard/Index.tsx
+++ resources/js/Pages/Dashboard/Index.tsx
@@ -1,9 +1,11 @@
 import DashboardLayout from '@/Layouts/DashboardLayout'
+import { Head } from '@inertiajs/react'
 import React from 'react'
 
 const Index = () => {
   return (
     <DashboardLayout>
+      <Head title="Dashboard" />
       <div className="text-3xl">Welcome To Dashboard</div>
     </DashboardLayout>
   )
