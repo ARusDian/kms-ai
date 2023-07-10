476b009ac8a35631bd2695d8b61a1184f46837a6
dimpram101 <dimas2004211@gmail.com>
2023-07-11 00:55:31 +0800

    style: restyled home's navbar logo

--- resources/js/Components/Home/Navbar.tsx
+++ resources/js/Components/Home/Navbar.tsx
@@ -1,3 +1,4 @@
+import { asset } from '@/Helper/document_file';
 import { Link } from '@inertiajs/react'
 import React from 'react'
 
@@ -6,13 +7,12 @@ const Navbar = () => {
 
   const HamburgerMenu = () => (
     <div className={`w-full h-full bg-white p-4 shadow-lg border-t z-40 transition-all  duration-300`}>
-      <div className="">
-        <div className="flex flex-col justify-around h-full gap-3">
-          <Link href='#' className=''>Home</Link>
-          <Link href='#' className=''>Fitur</Link>
-          <Link href='#' className=''>FAQ</Link>
-          <Link href='#' className=''>Lainnya</Link>
-        </div>
+      <div className="flex flex-col justify-around h-full gap-3">
+        <Link href='#' className=''>Home</Link>
+        <Link href='#' className=''>Fitur</Link>
+        <Link href='#' className=''>FAQ</Link>
+        <Link href='#' className=''>Lainnya</Link>
+        <Link href='#' className='font-bold text-primary'>Masuk</Link>
       </div>
     </div>
   )
@@ -20,8 +20,9 @@ const Navbar = () => {
   return (
     <nav className='fixed top-0 left-0 w-full h-24 bg-white shadow-sm z-50'>
       <div className="px-5 md:px-10 lg:px-20 mx-auto h-full flex flex-row justify-between items-center">
-        <h1 className='text-4xl font-bold font-sofia'>{"KMS."}</h1>
-        <div className="hidden md:flex flex-row w-[436px] text-[18px] md:justify-center md:gap-10  lg:justify-between ">
+        {/* <h1 className='text-4xl font-bold font-sofia'>{"KMS."}</h1> */}
+        <img src={asset('root', 'assets/logo-anset.png')} className='w-32 md:w-[8.5rem] lg:w-40' />
+        <div className="hidden md:flex flex-row w-[436px] text-[18px] md:justify-center md:gap-10  lg:justify-between items-center">
           <Link href='#' className='bg-left-bottom bg-gradient-to-r from-primary to-primary bg-[length:0%_1.5px] bg-no-repeat hover:bg-[length:100%_1.5px] transition-all duration-300 ease-out'>Home</Link>
           <Link href='#' className='bg-left-bottom bg-gradient-to-r from-primary to-primary bg-[length:0%_1.5px] bg-no-repeat hover:bg-[length:100%_1.5px] transition-all duration-300 ease-out'>Fitur</Link>
           <Link href='#' className='bg-left-bottom bg-gradient-to-r from-primary to-primary bg-[length:0%_1.5px] bg-no-repeat hover:bg-[length:100%_1.5px] transition-all duration-300 ease-out'>FAQ</Link>
@@ -35,8 +36,8 @@ const Navbar = () => {
           <div className={`border-2 border-white w-8 ${menuOpened && 'opacity-0'} transition-all duration-300`}></div>
           <div className={`border-2 border-white w-8 ${menuOpened && '-rotate-45 -translate-y-2.5'} transition-all duration-300`}></div>
         </div>
-      </div> 
-      <div className={`absolute w-40 h-40  ${menuOpened ? 'right-0' : '-right-96'} transition-all duration-300`}>
+      </div>
+      <div className={`absolute w-40 h-fit  ${menuOpened ? 'right-0' : '-right-96'} transition-all duration-300`}>
         <HamburgerMenu />
       </div>
     </nav>
