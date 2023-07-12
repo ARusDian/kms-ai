f2b0937950dff6ed794d0e6d6f3dabf1e993cea9
RUVNE <dian.galleon@gmail.com>
2023-07-12 19:13:13 +0800

    fix: add more prompt to chatgpt

--- resources/js/Pages/Dashboard/Assistant/AskGrowth.tsx
+++ resources/js/Pages/Dashboard/Assistant/AskGrowth.tsx
@@ -41,7 +41,7 @@ const AskGrowth = ({ ACCESS_TOKEN, CHATGPT_PROXY_URL, child }: Props) => {
     });
     const submitHandler = async () => {
         setIsAsking(true);
-        const response = await api.sendMessage("Anggaplah Dirimu sebagai orang yang bernama Ansel, Jawab Dalam Bahasa Indonesia\n\n" + formState.prompt).then((res) => {
+        const response = await api.sendMessage("Anggaplah Dirimu sebagai orang yang bernama Ansel, Jawab Dalam Bahasa Indonesia Mudah Dipahami\n\n" + formState.prompt).then((res) => {
             console.log(res);
             setFormState({
                 ...formState,
